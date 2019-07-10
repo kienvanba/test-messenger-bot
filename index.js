feca9231f26aa68412c9a09c59ad90247d4f5658
kien van ba <kien.vanba@gmail.com>
2019-07-10 16:41:27 +0700

    testing with fb messenger

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json());
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const request = require('request');
+
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.get('', (req, res) => {
@@ -16,6 +19,13 @@ app.post('/webhook', (req, res) => {
         body.entry.forEach(entry => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            let sender_id = webhook_event.sender.id;
+            showAction('mark_seen');
+            requestUserInfo(sender_id, user => {
+                console.log(user);
+            });
+            sendResponse(sender_id, {text: "hello!"});
         });
 
         res.status(200).send('EVENT_RECEIVED');
@@ -40,3 +50,61 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+sendResponse = (sender_id, response) => {
+    let requestBody = {
+        "recipient": {
+            "id": sender_id,
+        },
+        "message": response
+    }
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": requestBody
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('response to user success');
+        } else {
+            console.log(`response to user fail: ${err}`);
+        }
+    });
+}
+
+requestUserInfo = (sender_id, callback) => {
+    request({
+        "uri": `https://graph.facebook.com/${sender_id}`,
+        "qs": { "fields": "id, name, first_name, last_name, profile_pic"},
+        "method": "GET"
+    }, (err, res, body) => {
+        if (!err) {
+            callback(body);
+        } else {
+            console.log(`request user info fail: ${err}`);
+        }
+    });
+}
+
+showAction = (action) => {
+    let requestBody = {
+        "recipient": {
+            "id": sender_id,
+        },
+        "sender_action": action
+    }
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": requestBody
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('response to user success');
+        } else {
+            console.log(`response to user fail: ${err}`);
+        }
+    });
+}
