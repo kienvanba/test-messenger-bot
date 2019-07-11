3fd2f2b46ba078cedefecd878c7e094573d7ff9a
kien van ba <kien.vanba@gmail.com>
2019-07-11 10:59:28 +0700

    test user info

--- index.js
+++ index.js
@@ -38,24 +38,27 @@ app.post('/webhook', (req, res) => {
             console.log(webhook_event);
 
             let sender_psid = webhook_event.sender.id;
-            showAction(sender_psid, 'mark_seen');
-            sendResponse(sender_psid, {text: "i'm a hacker"});
-            showAction(sender_psid, 'typing_on');
+            // showAction(sender_psid, 'mark_seen');
+            // sendResponse(sender_psid, {text: "i'm a hacker"});
+            // showAction(sender_psid, 'typing_on');
             requestUserInfo(sender_psid, user => {
-                showAction(sender_psid, 'typing_off');
-                let response = {};
-                var payload = {};
-                payload['template_type'] = "generic";
-                payload['elements'] = [{
+                // showAction(sender_psid, 'typing_off');
+                let user_template = {
                     title: user.name,
                     image_url: user.profile_pic,
                     subtitle: 'this account is now hacked',
-                    buttons: []
-                }];
-                var attachment = {};
-                attachment['type'] = "template";
-                attachment['payload'] = payload;
-                response['attachment'] = attachment;
+                };
+                let response = {
+                    attachment: {
+                        type: 'template',
+                        payload: {
+                            template_type: 'generic',
+                            elements: [user_template]
+                        }
+                    }
+                };
+                console.log(user);
+                console.log(response);
                 sendResponse(sender_psid, response);
             });
             // console.log(`received message from ${sender_psid}`);
